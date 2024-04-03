37a1036e48bc9afd6408a8e4bf37346f3044cdb4
VictorMartial <fanomezanvm@gmail.com>
2024-04-03 23:33:13 +0300

    surcharge du methode equals

--- tp_test/src/tp_test/Money.java
+++ tp_test/src/tp_test/Money.java
@@ -6,4 +6,15 @@ public Money(int amount, String currency) { fAmount = amount; fCurrency = curren
 public int amount() { return fAmount; } 
 public String currency() { return fCurrency; } 
 public Money add(Money m) { return new Money(amount() + m.amount(), currency()); } 
+
+public boolean equals(Object ob) {
+	if(this==ob) {
+		return false;
+	}
+	if(ob == null || getClass() != ob.getClass()){
+		return false;
+	}
+	Money autre= (Money) ob;
+	return fAmount==autre.fAmount && fCurrency.equals(autre.fCurrency);
+}
 } 
